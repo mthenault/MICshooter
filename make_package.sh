515b6f86cd578e331eaee147af688c45c99b7e10
Michael Thenault <michael.thenault@gmail.com>
2012-05-22 23:58:14 +0200

    m

--- make_package.sh
+++ make_package.sh
@@ -1,7 +1,8 @@
-rm -rf michaelJEU/
-svn export .  michaelJEU/
-tar -czpf michaelJEU.tar.gz michaelJEU/
-scp michaelJEU.tar.gz michael@thenault.hd.free.fr:/var/www/dossier_upload/
-rm -rf michaelJEU/
-rm michaelJEU.tar.gz
+#rm -rf michaelJEU/
+#svn export .  michaelJEU/
+#tar -czpf michaelJEU.tar.gz michaelJEU/
+git archive master | bzip2 > MICshooter.tar.bz2
+scp MICshooter.tar.bz2 michael@thenault.hd.free.fr:/var/www/dossier_upload/
+#rm -rf michaelJEU/
+rm MICshooter.tar.bz2
 echo "http://thenault.hd.free.fr/dossier_upload/michaelJEU.tar.gz"
